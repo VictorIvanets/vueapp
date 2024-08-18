f8357343b25fc349c89ea0cbf0f7cda009e03b59
unknown <imperia.zt@gmail.com>
2024-08-18 13:48:05 +0300

    del css

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,19 +1,15 @@
 <template>
 	<div class="auth">
 		<h2>Auth</h2>
-        <form>
-
-                <input type="text" placeholder="name">
-                <input type="text" placeholder="password">
-
-        </form>
-        <button class="auth__btn" @click="this.authWin()">auth</button>
-
+		<form>
+			<input type="text" placeholder="name" />
+			<input type="text" placeholder="password" />
+		</form>
+		<button class="auth__btn" @click="this.authWin()">auth</button>
 	</div>
 </template>
 
 <script>
-
 export default {
 	data() {
 		return {
